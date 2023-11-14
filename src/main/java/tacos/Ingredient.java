4db789fcaeca3f5b88583771d03ca5c088f31b5c
Nick <nick.latcham@proton.me>
2023-11-14 12:29:20 -0600

    feat(Ingredient): add Ingredient class

---
+++ src/main/java/tacos/Ingredient.java
@@ -0,0 +1,12 @@
+package tacos;
+
+import lombok.Data;
+import tacos.enums.Type;
+
+@Data
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+}
