884e0039d136812634a1ca2e4b96da07b38766b2
Nick Latcham <nick.latcham@proton.me>
2023-11-14 21:38:40 -0600

    feat: adds validation

--- src/main/java/tacos/web/controllers/DesignTacoController.java
+++ src/main/java/tacos/web/controllers/DesignTacoController.java
@@ -1,9 +1,11 @@
 package tacos.web.controllers;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.Errors;
 import tacos.dto.Ingredient;
 import tacos.dto.Taco;
 import tacos.dto.TacoOrder;
@@ -21,18 +23,7 @@ public class DesignTacoController {
 
     @ModelAttribute
     public void addIngredientsToModel(Model model) { // TODO: Change this to use ingredient data from db
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        List<Ingredient> ingredients = Arrays.asList(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP), new Ingredient("COTO", "Corn Tortilla", Type.WRAP), new Ingredient("GRBF", "Ground Beef", Type.PROTEIN), new Ingredient("CARN", "Carnitas", Type.PROTEIN), new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES), new Ingredient("LETC", "Lettuce", Type.VEGGIES), new Ingredient("CHED", "Cheddar", Type.CHEESE), new Ingredient("JACK", "Monterrey Jack", Type.CHEESE), new Ingredient("SLSA", "Salsa", Type.SAUCE), new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
 
         Type[] types = Type.values();
 
@@ -57,7 +48,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
 
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
@@ -66,9 +61,7 @@ public class DesignTacoController {
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-        return ingredients.stream()
-                          .filter(ingredient -> ingredient.getType().equals(type))
-                          .collect(Collectors.toList());
+        return ingredients.stream().filter(ingredient -> ingredient.getType().equals(type)).collect(Collectors.toList());
     }
 
 }
--- src/main/java/tacos/web/controllers/OrderController.java
+++ src/main/java/tacos/web/controllers/OrderController.java
@@ -1,10 +1,15 @@
 package tacos.web.controllers;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import tacos.dto.TacoOrder;
 
 @Slf4j
 @Controller
@@ -16,6 +21,17 @@ public class OrderController {
     public String orderForm() {
         return "orderForm";
     }
-}
 
-// TODO: Left off at pg.47(74) Listing 2.9 A taco order form view
+    @PostMapping
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+
+        if (errors.hasErrors()) { // TODO: Left off at 2.3.3 Displaying validation errors
+            return "orderForm";
+        }
+
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+}
